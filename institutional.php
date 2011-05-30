4c0365997ec57d849017945af3ad2152d2d16ca8
Alex Bilbie <alex.bilbie@gmail.com>
2011-05-30 18:33:34 +0100

    Removed the popup

--- institutional.php
+++ institutional.php
@@ -2,27 +2,25 @@
 		
 	<h1>Institutional URIs</h1>
 	
+		<h2>Assessing the situation</h2>
+						
+		<iframe style="width:100%;height:400px" frameborder='0' src='https://spreadsheets1.google.com/pub?hl=en_GB&hl=en_GB&key=0AuO04KEqnewAdC1mSzhYYXpMZWZfM2I5ZXpvb0JYWkE&single=true&gid=1&output=html&widget=true'></iframe>
+			
+		<p class="notice">This data was collected during the final days of May 2011 &mdash; institutions may have modified their URI structures since this point.</p>
+		
+		<p>The data represents key areas of a total of 40 higher education institutional websites, as well as highlighting the differences that institutions have in URI structure. We studied 10 institutions from four of the UK university collaboration groups &mdash; the 1994 Group, the Russell Group, the Alliance Group and the Million+ group.</p>
+		
+		<p>The red highlights represent where a website (at the time of the survey) goes against Linking You recommended practices, and where such a behaviour is heavily discouraged for technical or usability reasons. In the case of the "WWW Required" column it means that the site is not accessible without the <span class="cleanuri">www.</span> prefix (see <a href="http://lncn.eu/toolkit/domains">Domains</a> for more on this), and in the case of the "404 Redirect" column it means that the behaviour effectively breaks the internet's expected behaviour model.</p>
+		
+		<p>An orange highlight in the "WWW Required" column indicates that although the behaviour is technically sound and does not significantly impact the user's experience it still goes against the Linking You recommendations.</p>
+		
+		<p>Where a column has green highlights they indicate that the URI for that area follows the recommended Linking You "Cool URI". However, where a column is not highlighted it does not indicate a problem with the URI. In many cases different URIs exist for historical or local reasons and &mdash; whilst not what would be recommended for maximum cross-institution compatibility &mdash; are entirely valid.</p>
+		
 		<h2>Everyone Is Different</h2>
-	
-		<p>If you want, you can <a onclick="$('#modal_institution_survey').dialog('open');">see the source data</a> for this section.</p>
 		
-		<div id="modal_institution_survey" title="Institutional URIs Survey">
+		<p>
 			
-			<iframe style="width:100%;height:400px" frameborder='0' src='https://spreadsheets1.google.com/pub?hl=en_GB&hl=en_GB&key=0AuO04KEqnewAdC1mSzhYYXpMZWZfM2I5ZXpvb0JYWkE&single=true&gid=1&output=html&widget=true'></iframe>
-		
-			<h2 class="margin_top">About The Data</h2>
-			
-			<p>This data was collected during the final days of May 2011 &mdash; institutions may have modified their URI structures since this point.</p>
-			
-			<p>The data represents key areas of a total of 40 higher education institutional websites, as well as highlighting the differences that institutions have in URI structure.</p>
-			
-			<p>The red highlights represent where a website (at the time of the survey) goes against Linking You recommended practices, and where such a behaviour is heavily discouraged for technical or usability reasons. In the case of the "WWW Required" column it means that the site is not accessible without the <span class="cleanuri">www.</span> prefix (see <a href="http://lncn.eu/toolkit/domains">Domains</a> for more on this), and in the case of the "404 Redirect" column it means that the behaviour effectively breaks the internet's expected behaviour model.</p>
-			
-			<p>An orange highlight in the "WWW Required" column indicates that although the behaviour is technically sound and does not significantly impact the user's experience it still goes against the Linking You recommendations.</p>
-			
-			<p>Where a column has green highlights they indicate that the URI for that area follows the recommended Linking You "Cool URI". However, where a column is not highlighted it does not indicate a problem with the URI. In many cases different URIs exist for historical or local reasons and &mdash; whilst not what would be recommended for maximum cross-institution compatibility &mdash; are entirely valid.</p>
-		
-		</div>
+		</p>
 	
 	<!--<h2>Discuss This</h2>
 	
